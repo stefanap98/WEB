f528d40a0dd1c63793c58a9803dc5d4361c0cb59
stefanap98 <84773438+stefanap98@users.noreply.github.com>
2021-05-25 13:12:06 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	
-	$(".project").click(function() {
-		var id = $(this)[0].id;
-		window.location = "http://localhost/AppStoreProject/project.php?id=" + id;
-	});
-	
-});
